bc34f1ce425d0669fdb72c4853a5e15092a894cf
Simran Mandal <simranmandal0211@gmail.com>
2023-08-03 00:34:26 +0530

    Separate Theme Action

--- js/script.js
+++ js/script.js
@@ -1,52 +1,5 @@
 $(document).ready(function() {
 
-
-// --------------------Theme Changing-----------------------
-    // Retrieve the necessary elements
-    const body = $("body");
-    const toggleButton = $(".dl-toggler");
-    const toggleIcon = $("#toggleIcon");
-    const toggleImage = $("#toggleImage");
-
-    let isImage = true;
-
-    // Add event listener to the toggle button
-    toggleButton.on("click", function(){
-
-        toggleIcon.toggleClass("fa-moon fa-sun");
-
-        body.toggleClass("dark light");
-        $("header").toggleClass("dark light");
-        $("header h1").toggleClass("dark light");
-
-        $(".nav-buttons a").toggleClass("dark light");
-        $("#button2").toggleClass("dark light");
-
-        $(".main").toggleClass("dark light");
-        $(".search-bar").toggleClass("dark light");
-        $(".search-bar .search-icon").toggleClass("dark light");
-        $(".search-bar .search-bar-text").toggleClass("dark light");
-        
-        $(".add-to-fav-btn").toggleClass("dark light");
-        $(".fav-btns-bg").toggleClass("dark light");
-
-        
-
-        // toggle Image
-        if(isImage){
-            toggleImage.attr("src", "./images/darkBg.jpg");
-            toggleImage.attr("alt", "darkHeros");
-            isImage = false;
-        }else{
-            toggleImage.attr("src", "./images/lightBg.jpg");
-            toggleImage.attr("alt", "lightHeros");
-            isImage = true;
-        }
-    });
-
-
-
-
 // -----------------------Selecting the element from DOM-----------------------
 
     let searchBarInput = document.getElementById('search-bar-input');
